543d7ec3d1ccacbae064c6fc9ff0503c22b380b2
Wiebke <wickoby@gmail.com>
2018-10-08 15:13:30 +0200

    Added the intent to the BroadcastActivity.

--- app/src/main/java/broadcaster/asuper/superbroadcaster/MainActivity.java
+++ app/src/main/java/broadcaster/asuper/superbroadcaster/MainActivity.java
@@ -1,7 +1,10 @@
 package broadcaster.asuper.superbroadcaster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button broadcastButton = findViewById(R.id.BroadcastButton);
+        broadcastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent broadcastIntent = new Intent(MainActivity.this, BroadcastActivity.class);
+                startActivity(broadcastIntent);
+            }
+        });
     }
 }
