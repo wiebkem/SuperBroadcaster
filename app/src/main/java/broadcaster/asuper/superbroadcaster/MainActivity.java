da7a8a5cfe6db453894554780c068f2d5bd83ffb
Wiebke <wickoby@gmail.com>
2018-10-10 16:29:23 +0200

    Created layout.

--- app/src/main/java/broadcaster/asuper/superbroadcaster/MainActivity.java
+++ app/src/main/java/broadcaster/asuper/superbroadcaster/MainActivity.java
@@ -2,12 +2,25 @@ package broadcaster.asuper.superbroadcaster;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar seekbar;
+    private TextView startTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = findViewById(R.id.playButton);
+        Button pauseButton = findViewById(R.id.pauseButton);
+        Button backwardButton = findViewById(R.id.backButton);
+        Button forwardButton = findViewById(R.id.forwardButton);
+        seekbar = findViewById(R.id.seekBar);
+        startTime = findViewById(R.id.startTextView);
+        final TextView endTime = findViewById(R.id.endTextView);
     }
 }
