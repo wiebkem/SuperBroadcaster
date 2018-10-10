82d1f0036b45577a2006be918ae93158dab5f03c
Wiebke <wickoby@gmail.com>
2018-10-10 11:41:31 +0200

    Basic app.

--- app/src/main/java/broadcaster/asuper/superbroadcaster/MainActivity.java
+++ app/src/main/java/broadcaster/asuper/superbroadcaster/MainActivity.java
@@ -1,10 +1,7 @@
 package broadcaster.asuper.superbroadcaster;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button broadcastButton = findViewById(R.id.BroadcastButton);
-        broadcastButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent broadcastIntent = new Intent(MainActivity.this, BroadcastActivity.class);
-                startActivity(broadcastIntent);
-            }
-        });
     }
 }
