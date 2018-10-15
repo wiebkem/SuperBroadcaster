d3bca21966f9cdf3ac11a9c901ff6147b22943ac
Wiebke <wickoby@gmail.com>
2018-10-15 14:07:40 +0200

    Changed the snippet shelf fragment.

--- app/src/main/java/broadcaster/asuper/superbroadcaster/SnippetShelf.java
+++ app/src/main/java/broadcaster/asuper/superbroadcaster/SnippetShelf.java
@@ -1,23 +1,11 @@
 package broadcaster.asuper.superbroadcaster;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SnippetShelf.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SnippetShelf#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SnippetShelf extends Fragment {
 
     @Override
@@ -25,69 +13,4 @@ public class SnippetShelf extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_snippet_shelf, container, false);
     }
-
-    /*private OnFragmentInteractionListener mListener;
-
-    public SnippetShelf() {
-        // Required empty public constructor
-    }
-
-    public static SnippetShelf newInstance(String param1, String param2) {
-        SnippetShelf fragment = new SnippetShelf();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    /*public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
 }
