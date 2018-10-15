05029b33411b87ec5d2b9280ec9da0dd9bd13aeb
Wiebke <wickoby@gmail.com>
2018-10-15 11:39:14 +0200

    Working Bottom Navigation Activity.

--- app/src/main/java/broadcaster/asuper/superbroadcaster/MainActivity.java
+++ app/src/main/java/broadcaster/asuper/superbroadcaster/MainActivity.java
@@ -1,120 +1,47 @@
 package broadcaster.asuper.superbroadcaster;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.SeekBar;
+import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-    private Handler myHandler = new Handler();
-    public static int oneTimeOnly = 0;
-    private MediaPlayer mediaPlayer;
-    private SeekBar seekbar;
-    private TextView startTime;
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_myloan:
+                    mTextMessage.setText(R.string.myloan);
+                    return true;
+                case R.id.navigation_discover:
+                    mTextMessage.setText(R.string.Discover);
+                    return true;
+                case R.id.navigation_currentbook:
+                    mTextMessage.setText(R.string.Currentbook);
+                    return true;
+                case R.id.navigation_more:
+                    mTextMessage.setText(R.string.More);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button playButton = findViewById(R.id.playButton);
-        Button pauseButton = findViewById(R.id.pauseButton);
-        Button backwardButton = findViewById(R.id.backButton);
-        Button forwardButton = findViewById(R.id.forwardButton);
-        seekbar = findViewById(R.id.seekBar);
-        startTime = findViewById(R.id.startTextView);
-        final TextView endTime = findViewById(R.id.endTextView);
-
-        mediaPlayer = MediaPlayer.create(this, R.raw.peterpanch1);
-
-        final int duration = mediaPlayer.getDuration();
-        final int forwardTime = 5000; // 5 seconds
-        final int backwardTime = 5000; // 5 seconds
-
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaPlayer.start();
-
-                int currentPosition = mediaPlayer.getCurrentPosition();
-
-                if (oneTimeOnly == 0) {
-                    seekbar.setMax(duration);
-                    oneTimeOnly = 1;
-                }
-                seekbar.setProgress(currentPosition);
-
-                endTime.setText(
-                        String.format("%d min, %d sec", TimeUnit.MILLISECONDS.toMinutes((long) duration),
-                                TimeUnit.MILLISECONDS.toSeconds((long) duration) -
-                                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) duration)))
-                );
-
-                startTime.setText(String.format("%d min, %d sec",
-                        TimeUnit.MILLISECONDS.toMinutes((long) currentPosition),
-                        TimeUnit.MILLISECONDS.toSeconds((long) currentPosition) -
-                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
-                                        currentPosition)))
-                );
-
-                myHandler.postDelayed(UpdateSongTime,100);
-            }
-        });
-
-        pauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaPlayer.pause();
-            }
-        });
-
-        forwardButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int currentPosition = mediaPlayer.getCurrentPosition();
-
-                if((currentPosition + forwardTime) <= duration){
-                    currentPosition = currentPosition + forwardTime;
-                    mediaPlayer.seekTo(currentPosition);
-                }else{
-                    Toast.makeText(getApplicationContext(),"Cannot jump forward 5 seconds",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        backwardButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int currentPosition = mediaPlayer.getCurrentPosition();
-
-                if((currentPosition - backwardTime) > 0){
-                    currentPosition = currentPosition - backwardTime;
-                    mediaPlayer.seekTo(currentPosition);
-                }else{
-                    Toast.makeText(getApplicationContext(),"Cannot jump backward 5 seconds",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    private Runnable UpdateSongTime = new Runnable() {
-        public void run() {
-            int currentPosition = mediaPlayer.getCurrentPosition();
-            startTime.setText(String.format("%d min, %d sec",
-                    TimeUnit.MILLISECONDS.toMinutes((long) currentPosition),
-                    TimeUnit.MILLISECONDS.toSeconds((long) currentPosition) -
-                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
-                                    toMinutes((long) currentPosition)))
-            );
-            seekbar.setProgress(currentPosition);
-            myHandler.postDelayed(this, 100);
-        }
-    };
 }
