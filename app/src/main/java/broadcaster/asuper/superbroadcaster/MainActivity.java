9965b79bbc2939d4808b646148db0abf6770cbfa
Wiebke <wickoby@gmail.com>
2018-10-15 13:49:00 +0200

    Open the snippet shelf fragment in the view.

--- app/src/main/java/broadcaster/asuper/superbroadcaster/MainActivity.java
+++ app/src/main/java/broadcaster/asuper/superbroadcaster/MainActivity.java
@@ -3,6 +3,9 @@ package broadcaster.asuper.superbroadcaster;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,12 +13,15 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Fragment currentFragment;
+    private FragmentTransaction ft;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment currentFragment;
             switch (item.getItemId()) {
                 case R.id.navigation_myloan:
                     mTextMessage.setText(R.string.myloan);
@@ -26,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_currentbook:
                     mTextMessage.setText(R.string.Currentbook);
                     return true;
+                case R.id.navigation_snippetbookshelf:
+                    mTextMessage.setText(R.string.Snippetbookshelf);
+                    currentFragment = new SnippetShelf();
+                    loadFragment(currentFragment);
+                    return true;
                 case R.id.navigation_more:
                     mTextMessage.setText(R.string.More);
                     return true;
@@ -33,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
+
+    // load the specified fragment
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
