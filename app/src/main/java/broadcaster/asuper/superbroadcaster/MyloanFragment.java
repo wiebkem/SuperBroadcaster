f3f24a9db36adfa336cf9faebd0e8213d372b730
Wiebke <wickoby@gmail.com>
2018-10-17 14:51:30 +0200

    Fixed the my loan item on click listener.

--- app/src/main/java/broadcaster/asuper/superbroadcaster/MyloanFragment.java
+++ app/src/main/java/broadcaster/asuper/superbroadcaster/MyloanFragment.java
@@ -5,11 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -20,25 +22,25 @@ import java.util.List;
 
 public class MyloanFragment extends Fragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view =  inflater.inflate(R.layout.fragment_myloan2, container, false);
 
-        final List<AudioBooklist> myloan = new ArrayList<AudioBooklist>();
+        final List<AudioBooklist> myloan = new ArrayList<>();
         myloan.add(new AudioBooklist ("A Game of Thrones: Book One", "By: George R.R. Martin", "22 days left", R.drawable.books_0008 ));
         myloan.add(new AudioBooklist("Run for Your Life", "By: William Pullen", "14 day left", R.drawable.my_loans_book_2));
         myloan.add(new AudioBooklist("Astrophysics for People in a Hurry", "By: Neil deGrasse Tyson", "21 day left", R.drawable.my_loans_book_3));
 
         ListView listView = view.findViewById(R.id.mainMain);
-        ArrayAdapter<AudioBooklist> bookAdapter = new CustomMyloanAdapter (getActivity(), android.R.layout.simple_list_item_1, myloan.toArray());
-        listView.setAdapter(bookAdapter);
+        ArrayAdapter<AudioBooklist> customAdapter = new CustomMyloanAdapter (getActivity(), android.R.layout.simple_list_item_1, myloan.toArray());
+        listView.setAdapter(customAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("MyLoan", "Item On Click Event fired");
                 Intent intent = new Intent(getActivity(), MediaPlayerActivity.class);
                 startActivity(intent);
             }
