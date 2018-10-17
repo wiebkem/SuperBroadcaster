52dddd2f2269e3e32c2fa7d52ebd6dba7691820a
Wiebke <wickoby@gmail.com>
2018-10-17 13:42:36 +0200

    Added the before create snippet activity, create snippet activity and save snippet activity.

---
+++ app/src/main/java/broadcaster/asuper/superbroadcaster/BeforeCreateSnippetActivity.java
@@ -0,0 +1,13 @@
+package broadcaster.asuper.superbroadcaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeforeCreateSnippetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_before_create_snippet);
+    }
+}
---
+++ app/src/main/java/broadcaster/asuper/superbroadcaster/SaveSnippetActivity.java
@@ -0,0 +1,13 @@
+package broadcaster.asuper.superbroadcaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SaveSnippetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_snippet);
+    }
+}
