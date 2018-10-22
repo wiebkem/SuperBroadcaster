de44b0be81a6166993b3d64cffd3e9f08e079252
Wiebke <wickoby@gmail.com>
2018-10-22 14:18:42 +0200

    Changed the media player activity and added the button on click methods.

--- app/src/main/java/broadcaster/asuper/superbroadcaster/MediaPlayerActivity.java
+++ app/src/main/java/broadcaster/asuper/superbroadcaster/MediaPlayerActivity.java
@@ -2,22 +2,18 @@ package broadcaster.asuper.superbroadcaster;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
-import android.widget.TextView;
 
 public class MediaPlayerActivity extends AppCompatActivity {
-    Button button;
     private MediaPlayer mediaPlayer;
-    public TextView songName, songDetail1, songDetail2;
     private double timeElapsed = 0, finalTime = 0;
-    private int forwardTime = 2000, backwardTime= 2000;
+    private int forwardTime = 30000, backwardTime= 30000;
     private Handler durationHandler= new Handler();
     private SeekBar seekbar;
 
@@ -25,58 +21,87 @@ public class MediaPlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_player);
-        button = (Button)findViewById(R.id.button);
-            button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), BeforeCreateSnippetActivity.class);
-                startActivity(intent);
-            }
-        });
 
-          initializeViews();
-
-    }
-
-    public void initializeViews(){
-        songName= findViewById(R.id.songName);
-        mediaPlayer= MediaPlayer.create(this, R.raw.peterpanch1);
+        // create the mediaplayer
+        mediaPlayer = MediaPlayer.create(this, R.raw.peterpanch1);
+        // set the seekbar details
         finalTime = mediaPlayer.getDuration();
-        songDetail1= findViewById(R.id. songDetail1);
-        songDetail2= findViewById(R.id. songDetail2);
-        seekbar= findViewById(R.id.seekBar3);
-        songName.setText("A Game of Thrones");
-        songDetail1.setText("Book1 of A Song of Ice and Fire");
-        songDetail2.setText("Chapter 3");
+        seekbar = findViewById(R.id.seekBar);
         seekbar.setMax((int) finalTime);
         seekbar.setClickable(false);
 
+        RelativeLayout unsharedSnippetLayout = findViewById(R.id.mediaPlayerLayout);
+        unsharedSnippetLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MediaPlayerActivity.this, BeforeCreateSnippetActivity.class);
+                startActivity(myIntent);
+            }
+        });
 
+        final ImageButton playPauseButton = findViewById(R.id.playPauseButton);
+        playPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    pauseAudioBook();
+                    playPauseButton.setImageResource(R.drawable.pause);
+                } else {
+                    playAudioBook();
+                    playPauseButton.setImageResource(R.drawable.playing);
+                }
+            }
+        });
+
+        ImageButton forwardButton = findViewById(R.id.forwardButton);
+        forwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                forwardMediaPlayer();
+            }
+        });
+
+        ImageButton backwardButton = findViewById(R.id.backwardButton);
+        backwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backwardMediaPlayer();
+            }
+        });
     }
 
-    public void play (View view){
+    private void playAudioBook() {
         mediaPlayer.start();
-        timeElapsed= mediaPlayer.getCurrentPosition();
+
+        timeElapsed = mediaPlayer.getCurrentPosition();
         seekbar.setProgress((int) timeElapsed);
+
         durationHandler.postDelayed(updateSeekBarTime, 100);
     }
+
+    private void pauseAudioBook(){
+        mediaPlayer.pause();
+    }
+
     private Runnable updateSeekBarTime = new Runnable() {
         @Override
         public void run() {
-            timeElapsed = mediaPlayer.getCurrentPosition();
-            seekbar.setProgress((int) timeElapsed);
-            double timeRemaining = finalTime - timeElapsed;
-            durationHandler.postDelayed(this,100);
+        timeElapsed = mediaPlayer.getCurrentPosition();
+        seekbar.setProgress((int) timeElapsed);
+
+        double timeRemaining = finalTime - timeElapsed;
+        durationHandler.postDelayed(this,100);
         }
     };
-   public void pause (View view){
-       mediaPlayer.pause();
-   }
 
-   public void forward (View view){
+    private void forwardMediaPlayer() {
        if((timeElapsed + forwardTime) <= finalTime){
            timeElapsed= timeElapsed - backwardTime;
            mediaPlayer.seekTo((int) timeElapsed);
        }
-   }
+    }
+
+    private void backwardMediaPlayer() {
+        // TODO
+    }
 }
